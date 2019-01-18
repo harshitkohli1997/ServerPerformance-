61b9d3c842e7c68fe16894c8aeea2919ea38fc82
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-01-18 18:30:34 +0530

    react app setup

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://advancednode:scooby1234@ds217349.mlab.com:17349/mongouploads',
   cookieKey: '123123123'
 };
--- index.js
+++ index.js
@@ -10,7 +10,10 @@ require('./models/Blog');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, { useMongoClient: true })
+.then(() => {
+  console.log('db connected');
+})
 
 const app = express();
 
