ec576413bea11647d7bab08091c13fdbe1d9e377
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-01-24 20:07:10 +0530

    redis server setup

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,30 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis');
 
+    const client = redis.createClient();
+
+    
+    const util = require('util');
+    client.get = util.promisify(client.get);
+    //if cached data is available in redis return it from redis server 
+    //without hammering mongo server
+    const cachedBlogs = await client.get(req.user.id);
+
+    if(cachedBlogs) {
+      console.log('serving from cache')
+      return res.send(JSON.parse(cachedBlogs));
+    }
+   //if no data is available req mongo server and update cached server
+    
+
+    const blogs = await Blog.find({ _user: req.user.id });
+   console.log('serving from mongo server');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
+    
+  
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
