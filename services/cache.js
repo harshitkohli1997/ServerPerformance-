a002b355c4915339e4c4c9ef87a0c32b8d6537ba
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-02-05 17:42:39 +0530

    query details

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,7 @@ const exec = mongoose.Query.prototype.exec;
 //overwriting more functionality to mongoose query function
 mongoose.Query.prototype.exec = function() {
     console.log(`I'm' about to run a query`);
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
     return exec.apply(this,arguments);
 }
