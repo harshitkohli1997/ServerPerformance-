33f2f8a16eb670b67ce9a6c44bc034cfbd7c5a8b
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-02-05 17:46:15 +0530

    redis added to diff file

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
+const redis = require('redis');
+const util = require('util');
+
+const client = redis.createClient();
+
+client.get = util.promisify(client.get);
+
 //overwriting more functionality to mongoose query function
 mongoose.Query.prototype.exec = function() {
     console.log(`I'm' about to run a query`);
     console.log(this.getQuery());
     console.log(this.mongooseCollection.name);
+
+   const key = JSOM.stringify( Object.assign({},this.getQuery(), {
+        collection:this.mongooseCollection.name
+    })
+   );
+    console.log(key);
     return exec.apply(this,arguments);
 }
