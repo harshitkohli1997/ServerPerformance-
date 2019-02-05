1ac71d8c9f90185b1ae08627a922cccf4fd2b5d3
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-02-05 17:35:09 +0530

    redis code deleted

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,28 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
+   const blogs = await Blog.find({_user:req.user.id})
 
-    const client = redis.createClient();
-
-    
-    const util = require('util');
-    client.get = util.promisify(client.get);
-    //if cached data is available in redis return it from redis server 
-    //without hammering mongo server
-    const cachedBlogs = await client.get(req.user.id);
-
-    if(cachedBlogs) {
-      console.log('serving from cache')
-      return res.send(JSON.parse(cachedBlogs));
-    }
-   //if no data is available req mongo server and update cached server
-    
-//cache block
-    const blogs = await Blog.find({ _user: req.user.id });
-   console.log('serving from mongo server');
-    res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs));
+   res.send(blogs);
     
   
   });
