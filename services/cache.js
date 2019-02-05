a8a3214dc9a0149b5d79e18c119af5e8ebf04ec0
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-02-05 18:07:07 +0530

    cache server added

--- services/cache.js
+++ services/cache.js
@@ -9,15 +9,30 @@ const client = redis.createClient();
 client.get = util.promisify(client.get);
 
 //overwriting more functionality to mongoose query function
-mongoose.Query.prototype.exec = function() {
+mongoose.Query.prototype.exec =  async function() {
     console.log(`I'm' about to run a query`);
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+    // console.log(this.getQuery());
+    // console.log(this.mongooseCollection.name);
 
-   const key = JSOM.stringify( Object.assign({},this.getQuery(), {
+   const key = JSON.stringify( Object.assign({},this.getQuery(), {
         collection:this.mongooseCollection.name
     })
    );
-    console.log(key);
-    return exec.apply(this,arguments);
+
+   const cacheValue = await client.get(key);
+
+   if(cacheValue){
+       console.log(cacheValue);
+       const doc = JSON.parse(cacheValue);
+                
+    return  Array.isArray(doc)
+    ? doc.map(d => new this.model(d)) 
+    :new this.model(doc);
+       
+    }
+    // console.log(key);
+    const result = await exec.apply(this,arguments);
+    client.set(key,JSON.stringify(result));
+    return result;
+    console.log(result);
 }
