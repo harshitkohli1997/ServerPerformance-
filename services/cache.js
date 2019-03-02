964e2f1ade75f232703e042e17503ba4e4dd1b5f
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-03-02 17:49:20 +0530

    toggleable cache fn

--- services/cache.js
+++ services/cache.js
@@ -8,8 +8,17 @@ const client = redis.createClient();
 
 client.get = util.promisify(client.get);
 
+mongoose.Query.prototype.cache = function(){
+    this._cache = true;
+    return this;
+}
+
 //overwriting more functionality to mongoose query function
 mongoose.Query.prototype.exec =  async function() {
+    console.log(this._cache);
+    if(!this._cache){
+        return  exec.apply(this,arguments);
+     }
     console.log(`I'm' about to run a query`);
     // console.log(this.getQuery());
     // console.log(this.mongooseCollection.name);
@@ -18,11 +27,11 @@ mongoose.Query.prototype.exec =  async function() {
         collection:this.mongooseCollection.name
     })
    );
-
+    console.log(key);
    const cacheValue = await client.get(key);
 // if we have value for key in redis return that
    if(cacheValue){
-       console.log(cacheValue);
+       //console.log(cacheValue);
        const doc = JSON.parse(cacheValue);
            //if doc is array then parse each in array into model     
     return  Array.isArray(doc)
