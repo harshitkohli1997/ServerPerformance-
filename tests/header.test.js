e4e5ca6fe9f32fe283ab8a088cf85d2aaed21a51
harshitkohli1997 <harshit.kohli1997@gmail.com>
2019-03-20 04:12:44 +0530

    before each added

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,16 @@
 
 const puppeteer = require('puppeteer');
-
-test('Adds two numbers', () => {
-    const sum = 1+2;
-
-    expect(sum).toEqual(3);
-
+let browser,page;
+beforeEach(async () => {
+     browser = await puppeteer.launch({
+        headless:false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
 });
 
 test('we can launch a browser', async () => {
-    const browser = await puppeteer.launch({
-        headless:false
-    });
-    const page = await browser.newPage();
-    await page.goto('localhost:3000');
+    
 
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
